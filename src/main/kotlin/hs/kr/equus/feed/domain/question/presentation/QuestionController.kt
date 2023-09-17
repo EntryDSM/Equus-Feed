099fa5263910bfc833936222f0e48947a797e21f
HyunSu1768 <azxcv1768@gmail.com>
2023-09-17 18:12:47 +0900

    ♻️ :: (ENTRY-40) 디버깅 코드 삭제

--- src/main/kotlin/hs/kr/equus/feed/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/question/presentation/QuestionController.kt
@@ -2,8 +2,6 @@ package hs.kr.equus.feed.domain.question.presentation
 
 import hs.kr.equus.feed.domain.question.presentation.dto.request.CreateQuestionRequest
 import hs.kr.equus.feed.domain.question.service.CreateQuestionService
-import hs.kr.equus.feed.global.exception.ExpiredTokenException
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,10 +19,5 @@ class QuestionController(
         createQuestionRequest: CreateQuestionRequest
     ) {
         createQuestionService.execute(createQuestionRequest)
-    }
-
-    @GetMapping
-    fun test() {
-        throw ExpiredTokenException
     }
 }
