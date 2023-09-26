2ad3cfde5f42be2e68b1ab5697c42e7c09f79aa0
HyunSu1768 <azxcv1768@gmail.com>
2023-09-26 19:58:26 +0900

    ✨ :: (ENTRY-45) FeignClient 추가

---
+++ src/main/kotlin/hs/kr/equus/feed/infrastructure/feign/client/user/UserFeignClient.kt
@@ -0,0 +1,13 @@
+package hs.kr.equus.feed.infrastructure.feign.client.user
+
+import hs.kr.equus.feed.infrastructure.feign.client.user.dto.response.UserResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.util.UUID
+
+@FeignClient(name = "UserClient", url = "\${url.user}")
+interface UserFeignClient {
+    @GetMapping("/user")
+    fun getUserByUUID(@RequestParam("userId") userId: UUID) : UserResponse
+}
