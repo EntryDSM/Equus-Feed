2078a588a695502988fa6fb3f3400b0041205d23
HyunSu1768 <azxcv1768@gmail.com>
2023-09-26 19:59:40 +0900

    ✨ :: (ENTRY-45) Question 전체 조회 기능 추가

--- src/main/kotlin/hs/kr/equus/feed/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/question/presentation/QuestionController.kt
@@ -1,7 +1,10 @@
 package hs.kr.equus.feed.domain.question.presentation
 
 import hs.kr.equus.feed.domain.question.presentation.dto.request.CreateQuestionRequest
+import hs.kr.equus.feed.domain.question.presentation.dto.response.QuestionListResponse
 import hs.kr.equus.feed.domain.question.service.CreateQuestionService
+import hs.kr.equus.feed.domain.question.service.QueryQuestionListService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,7 +14,8 @@ import javax.validation.Valid
 @RequestMapping("/question")
 @RestController
 class QuestionController(
-    private val createQuestionService: CreateQuestionService
+    private val createQuestionService: CreateQuestionService,
+    private val queryQuestionListService: QueryQuestionListService
 ) {
     @PostMapping
     fun createQuestion(
@@ -20,4 +24,7 @@ class QuestionController(
     ) {
         createQuestionService.execute(createQuestionRequest)
     }
+
+    @GetMapping("/all")
+    fun getQuestionList() : QuestionListResponse = queryQuestionListService.execute()
 }
