363fc36e961e1d5cdfa58131a53abd3d34bffa69
HyunSu1768 <azxcv1768@gmail.com>
2023-09-26 19:59:15 +0900

    🐛 :: (ENTRY-45) 코드 수정

--- src/main/kotlin/hs/kr/equus/feed/domain/BaseEntity.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/BaseEntity.kt
@@ -9,11 +9,12 @@ import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 abstract class BaseEntity(
-    id: UUID?
-) : BaseTimeEntity() {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)", nullable = false)
-    val id: UUID? = if (id == UUID(0, 0)) null else id
-}
+    @Column(
+        columnDefinition = "BINARY(16)",
+        nullable = false
+    )
+    val id: UUID?
+) : BaseTimeEntity()
