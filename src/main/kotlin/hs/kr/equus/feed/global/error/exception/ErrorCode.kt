ebcb6f575842b42d2f1e04e5d2db9d3a8083f0c1
HyunSu1768 <azxcv1768@gmail.com>
2023-09-26 19:58:08 +0900

    ✨ :: (ENTRY-45) ErrorCode 추가

--- src/main/kotlin/hs/kr/equus/feed/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/error/exception/ErrorCode.kt
@@ -4,10 +4,17 @@ enum class ErrorCode(
     val status: Int,
     val message: String
 ) {
+    // Feign
+    FEIGN_BAD_REQUEST(400, "Feign Bad Request"),
+    FEIGN_UNAUTHORIZED(401, "Feign UnAuthorized"),
+    FEIGN_FORBIDDEN(403, "Feign Forbidden"),
+    FEIGN_SERVER_ERROR(500, "Feign Server Error"),
+
     // Internal Server Error
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
 
     // UnAuthorization
     INVALID_TOKEN(401, "Invalid Token"),
     EXPIRED_TOKEN(401, "Expired Token")
+
 }
