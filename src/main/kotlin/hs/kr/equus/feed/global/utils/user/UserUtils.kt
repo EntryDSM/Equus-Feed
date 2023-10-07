e05a896852418ee1d1439cc9c76a0caaa7eb2230
HyunSu1768 <azxcv1768@gmail.com>
2023-10-07 20:54:26 +0900

    feat :: Add GetCurrentUserIdAndRole

--- src/main/kotlin/hs/kr/equus/feed/global/utils/user/UserUtils.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/utils/user/UserUtils.kt
@@ -1,9 +1,18 @@
 package hs.kr.equus.feed.global.utils.user
 
+import hs.kr.equus.feed.global.utils.user.dto.UserInfo
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class UserUtils {
-    fun getCurrentUserId(): String = SecurityContextHolder.getContext().authentication.name
+    fun getCurrentUserId(): UUID = UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+
+    fun getCurrentUserRole(): String = SecurityContextHolder.getContext().authentication.authorities.toString()
+
+    fun getCurrentUserIdAndRole(): UserInfo = UserInfo(
+        userId = getCurrentUserId(),
+        userRole = getCurrentUserRole()
+    )
 }
