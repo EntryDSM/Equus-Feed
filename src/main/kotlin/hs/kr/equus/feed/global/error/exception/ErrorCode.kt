b08db5f8c081bea00aa231517de89bdbb59b138f
HyunSu1768 <azxcv1768@gmail.com>
2023-10-11 07:35:27 +0900

    feat :: Add Exception

--- src/main/kotlin/hs/kr/equus/feed/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/error/exception/ErrorCode.kt
@@ -19,6 +19,7 @@ enum class ErrorCode(
 
     // Forbidden
     ACCESS_DENIED_QUESTION(403, "No Permission To Access Question"),
+    ACCESS_DENIED_REPLY(403, "No Permission To Comment Question"),
     FEED_WRITER_MISMATCH(403, "Feed Writer Mismatch"),
 
     // Not Found
