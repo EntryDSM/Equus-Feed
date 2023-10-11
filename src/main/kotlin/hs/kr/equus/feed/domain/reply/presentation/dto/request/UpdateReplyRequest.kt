7023096ba0a2eaff2b78713583780d761d7a2781
HyunSu1768 <azxcv1768@gmail.com>
2023-10-11 23:05:22 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/dto/request/UpdateReplyRequest.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/dto/request/UpdateReplyRequest.kt
@@ -10,5 +10,5 @@ data class UpdateReplyRequest(
 
     @field:NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @field:Size(max = 5000, message = "content는 최대 5000자까지 가능합니다.")
-    val content: String,
+    val content: String
 )
