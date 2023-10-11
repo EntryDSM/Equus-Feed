1bccfd95b5fb4bc083d7057fe5f2878c90a094bc
HyunSu1768 <azxcv1768@gmail.com>
2023-10-11 21:28:29 +0900

    feat :: Add ValidationMessages.properties

--- src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/dto/request/UpdateReplyRequest.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/dto/request/UpdateReplyRequest.kt
@@ -4,11 +4,11 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 data class UpdateReplyRequest(
-    @field:NotBlank(message = "title은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @field:Size(max = 100, message = "title은 최대 100자까지 가능합니다.")
+    @field:NotBlank(message = "{title.notBlank}")
+    @field:Size(max = 100, message = "{title.size}")
     val title: String,
 
-    @field:NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @field:Size(max = 5000, message = "content는 최대 5000자까지 가능합니다.")
+    @field:NotBlank(message = "{content.notBlank}")
+    @field:Size(max = 5000, message = "{content.size}")
     val content: String
 )
