a7c0fd159868db75120bbcb4e6d2b58c6d84d7c0
HyunSu1768 <azxcv1768@gmail.com>
2023-10-12 21:55:57 +0900

    feat :: Add delete reply api

--- src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/ReplyController.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/presentation/ReplyController.kt
@@ -3,13 +3,17 @@ package hs.kr.equus.feed.domain.reply.presentation
 import hs.kr.equus.feed.domain.reply.presentation.dto.request.CreateReplyRequest
 import hs.kr.equus.feed.domain.reply.presentation.dto.request.UpdateReplyRequest
 import hs.kr.equus.feed.domain.reply.service.CreateReplyService
+import hs.kr.equus.feed.domain.reply.service.DeleteReplyService
 import hs.kr.equus.feed.domain.reply.service.UpdateReplyService
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -17,8 +21,10 @@ import java.util.UUID
 @RestController
 class ReplyController(
     private val createReplyService: CreateReplyService,
-    private val updateReplyService: UpdateReplyService
+    private val updateReplyService: UpdateReplyService,
+    private val deleteReplyService: DeleteReplyService
 ) {
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{questionId}")
     fun createReply(
         @RequestBody @Validated
@@ -26,10 +32,17 @@ class ReplyController(
         @PathVariable("questionId") questionId: UUID
     ) = createReplyService.execute(createReplyRequest, questionId)
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{replyId}")
     fun updateReply(
         @RequestBody @Validated
         updateReplyRequest: UpdateReplyRequest,
         @PathVariable("replyId") replyId: UUID
     ) = updateReplyService.execute(updateReplyRequest, replyId)
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{replyId}")
+    fun deleteReply(
+        @PathVariable("replyId") replyId: UUID
+    ) = deleteReplyService.execute(replyId)
 }
---
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/service/DeleteReplyService.kt
@@ -0,0 +1,19 @@
+package hs.kr.equus.feed.domain.reply.service
+
+import hs.kr.equus.feed.domain.reply.domain.repository.ReplyRepository
+import hs.kr.equus.feed.domain.reply.exception.ReplyNotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class DeleteReplyService(
+    private val replyRepository: ReplyRepository
+) {
+    @Transactional
+    fun execute(replyId: UUID){
+        val reply = replyRepository.findByIdOrNull(replyId) ?: throw ReplyNotFoundException
+        replyRepository.delete(reply)
+    }
+}
