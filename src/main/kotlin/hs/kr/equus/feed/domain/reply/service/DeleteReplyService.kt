7b253c85bbdbb7a9279deb503ea2f04a5edff71d
HyunSu1768 <azxcv1768@gmail.com>
2023-10-15 22:04:08 +0900

    refactor :: Change Exception

--- src/main/kotlin/hs/kr/equus/feed/domain/reply/service/DeleteReplyService.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/service/DeleteReplyService.kt
@@ -1,6 +1,7 @@
 package hs.kr.equus.feed.domain.reply.service
 
 import hs.kr.equus.feed.domain.question.domain.repository.QuestionRepository
+import hs.kr.equus.feed.domain.question.exception.QuestionNotFoundException
 import hs.kr.equus.feed.domain.reply.domain.repository.ReplyRepository
 import hs.kr.equus.feed.domain.reply.exception.ReplyNotFoundException
 import org.springframework.data.repository.findByIdOrNull
@@ -16,7 +17,7 @@ class DeleteReplyService(
     @Transactional
     fun execute(replyId: UUID) {
         val reply = replyRepository.findByIdOrNull(replyId) ?: throw ReplyNotFoundException
-        questionRepository.findByIdOrNull(reply.questionId) ?.updateIsReplied(false) ?: throw ReplyNotFoundException
+        questionRepository.findByIdOrNull(reply.questionId) ?.updateIsReplied(false) ?: throw QuestionNotFoundException
         replyRepository.delete(reply)
     }
 }
