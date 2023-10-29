1690cc6e25602db6fe63008d97474bb0f8ce2139
HyunSu1768 <azxcv1768@gmail.com>
2023-10-29 21:08:44 +0900

    feat :: Add CreateBannerLinkService

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/banner/service/CreateBannerLinkService.kt
@@ -0,0 +1,20 @@
+package hs.kr.equus.feed.domain.banner.service
+
+import hs.kr.equus.feed.domain.banner.domain.BannerLink
+import hs.kr.equus.feed.domain.banner.domain.repository.BannerLinkRepository
+import hs.kr.equus.feed.global.utils.s3.S3Utils
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
+
+@Service
+class CreateBannerLinkService(
+    private val bannerLinkRepository: BannerLinkRepository,
+    private val s3Utils: S3Utils
+) {
+    @Transactional
+    fun execute(file: MultipartFile): String{
+        bannerLinkRepository.save(BannerLink(link = file.name))
+        return s3Utils.generateObjectUrl(file.name)
+    }
+}
