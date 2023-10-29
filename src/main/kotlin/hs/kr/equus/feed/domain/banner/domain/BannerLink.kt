6527017b83dd0f800ffeb6f0507b6c91fda4f9d5
HyunSu1768 <azxcv1768@gmail.com>
2023-10-29 20:59:47 +0900

    feat :: Add Entity

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/banner/domain/BannerLink.kt
@@ -0,0 +1,14 @@
+package hs.kr.equus.feed.domain.banner.domain
+
+import hs.kr.equus.feed.domain.BaseEntity
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity(name = "tbl_banner_link")
+class BannerLink(
+    id: UUID? = null,
+
+    @Column(name = "link", nullable = false)
+    val link: String
+): BaseEntity(id)
