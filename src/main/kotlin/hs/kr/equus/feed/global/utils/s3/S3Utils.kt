346941606b9c94c1e79d60581cf8bee1fbb99a0e
도경 <dokyungkim2926@gmail.com>
2023-11-12 16:31:33 +0900

    feat :: Add Upload Function

--- src/main/kotlin/hs/kr/equus/feed/global/utils/s3/S3Utils.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/utils/s3/S3Utils.kt
@@ -1,5 +1,10 @@
 package hs.kr.equus.feed.global.utils.s3
 
+import org.springframework.web.multipart.MultipartFile
+
 interface S3Utils {
+
+    fun upload(file : MultipartFile, path : String) : String
+
     fun generateObjectUrl(fileName: String): String
 }
