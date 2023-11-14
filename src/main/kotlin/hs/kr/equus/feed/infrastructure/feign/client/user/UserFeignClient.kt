9aed1a272e5d09caf7f313509e0f1bb67a1e8c1d
HyunSu1768 <azxcv1768@gmail.com>
2023-11-14 15:22:02 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/feed/infrastructure/feign/client/user/UserFeignClient.kt
+++ src/main/kotlin/hs/kr/equus/feed/infrastructure/feign/client/user/UserFeignClient.kt
@@ -4,7 +4,6 @@ import hs.kr.equus.feed.infrastructure.feign.client.user.model.User
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
 import java.util.UUID
 
 @FeignClient(name = "UserClient", url = "\${url.user}")
