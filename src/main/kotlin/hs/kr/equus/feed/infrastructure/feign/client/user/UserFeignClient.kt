ea95a5742a1361d4de807e4419eb8da3f6bc5e22
HyunSu1768 <azxcv1768@gmail.com>
2023-11-14 14:15:42 +0900

    fix :: RequestParam to PathVariable

--- src/main/kotlin/hs/kr/equus/feed/infrastructure/feign/client/user/UserFeignClient.kt
+++ src/main/kotlin/hs/kr/equus/feed/infrastructure/feign/client/user/UserFeignClient.kt
@@ -3,11 +3,12 @@ package hs.kr.equus.feed.infrastructure.feign.client.user
 import hs.kr.equus.feed.infrastructure.feign.client.user.model.User
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.UUID
 
 @FeignClient(name = "UserClient", url = "\${url.user}")
 interface UserFeignClient {
-    @GetMapping("/user")
-    fun getUserByUUID(@RequestParam("userId") userId: UUID): User
+    @GetMapping("/user/{userId}")
+    fun getUserByUUID(@PathVariable userId: UUID): User
 }
