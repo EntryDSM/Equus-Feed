3f3b3aa2ae363094ab59da9f0c425f81988fef33
도경 <dokyungkim2926@gmail.com>
2023-11-30 19:56:28 +0900

    feat :: Add DeleteQuestionTableService

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/question/service/DeleteQuestionTableConsumerService.kt
@@ -0,0 +1,16 @@
+package hs.kr.equus.feed.domain.question.service
+
+import hs.kr.equus.feed.domain.question.domain.repository.QuestionRepository
+import hs.kr.equus.feed.infrastructure.kafka.configuration.KafkaTopics
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteQuestionTableConsumerService(
+    private val questionRepository: QuestionRepository
+) {
+    @Transactional
+    @KafkaListener(topics = [KafkaTopics.DELETE_ALL_TABLE], groupId = "\${kafka.consumer.groupId}")
+    fun execute() = questionRepository.deleteAll()
+}
