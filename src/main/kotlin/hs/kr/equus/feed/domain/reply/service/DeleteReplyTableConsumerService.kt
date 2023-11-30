668c13ebaab37e9a5722e10bb5e77ca557041033
도경 <dokyungkim2926@gmail.com>
2023-11-30 19:56:00 +0900

    feat :: Add DeleteReplyTableService

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/reply/service/DeleteReplyTableConsumerService.kt
@@ -0,0 +1,17 @@
+package hs.kr.equus.feed.domain.reply.service
+
+import hs.kr.equus.feed.domain.reply.domain.repository.ReplyRepository
+import hs.kr.equus.feed.infrastructure.kafka.configuration.KafkaTopics
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class DeleteReplyTableConsumerService(
+    private val replyRepository: ReplyRepository
+) {
+
+    @Transactional
+    @KafkaListener(topics = [KafkaTopics.DELETE_ALL_TABLE], groupId = "\${kafka.consumer.groupId}")
+    fun execute() = replyRepository.deleteAll()
+}
