3e825d67ade38545d49090d0c153c2fc39e103e5
도경 <dokyungkim2926@gmail.com>
2024-03-10 15:30:23 +0900

    add :: query faq title and type api

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/faq/presentation/dto/response/FaqTitleAndTypeResponse.kt
@@ -0,0 +1,10 @@
+package hs.kr.equus.feed.domain.faq.presentation.dto.response
+
+import hs.kr.equus.feed.domain.faq.domain.type.FaqType
+import java.util.UUID
+
+data class FaqTitleAndTypeResponse (
+    val id: UUID,
+    val type: FaqType,
+    val title: String
+)
