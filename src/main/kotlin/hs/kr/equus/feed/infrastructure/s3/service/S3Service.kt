7741c41cb716b5d30a985a3725a4be0f1e6a2a54
도경 <dokyungkim2926@gmail.com>
2024-03-10 16:54:43 +0900

    add :: s3 service

---
+++ src/main/kotlin/hs/kr/equus/feed/infrastructure/s3/service/S3Service.kt
@@ -0,0 +1,93 @@
+package hs.kr.equus.feed.infrastructure.s3.service
+
+import com.amazonaws.HttpMethod
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import hs.kr.equus.feed.infrastructure.s3.exception.BadFileExtensionException
+import hs.kr.equus.feed.infrastructure.s3.exception.EmptyFileException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.util.Date
+import java.util.Locale
+import java.util.UUID
+
+@Service
+class S3Service(
+    private val amazonS3: AmazonS3
+) {
+    @Value("\${spring.cloud.aws.s3.bucket}")
+    lateinit var bucketName: String
+
+    companion object {
+        const val EXP_TIME = 1000 * 60 * 2
+        const val PATH: String = "notice/"
+    }
+
+    fun upload(files: List<MultipartFile>): List<String> {
+        val uploadedFileNames = mutableListOf<String>()
+
+        files.forEach { file ->
+            val ext = verificationFile(file)
+
+            val randomName = UUID.randomUUID().toString()
+            val filename = "$randomName.$ext"
+            val inputStream: InputStream = ByteArrayInputStream(file.bytes)
+
+            val metadata = ObjectMetadata().apply {
+                contentType = MediaType.IMAGE_PNG_VALUE
+                contentLength = file.size
+                contentDisposition = "inline"
+            }
+
+            inputStream.use {
+                amazonS3.putObject(
+                    PutObjectRequest(bucketName, "${PATH}$filename", it, metadata)
+                        .withCannedAcl(CannedAccessControlList.AuthenticatedRead)
+                )
+            }
+
+            uploadedFileNames.add(filename)
+        }
+
+        return generateObjectUrl(uploadedFileNames)
+    }
+
+
+    fun generateObjectUrl(fileNames: List<String>): List<String> {
+        val expiration = Date().apply {
+            time += S3Service.EXP_TIME
+        }
+
+        val url = mutableListOf<String>()
+
+        fileNames.forEach { fileName ->
+            val urls = amazonS3.generatePresignedUrl(
+                GeneratePresignedUrlRequest(
+                    bucketName,
+                    "${PATH}$fileName"
+                ).withMethod(HttpMethod.GET).withExpiration(expiration)
+            ).toString()
+
+            url.add(urls)
+        }
+
+        return url
+    }
+
+
+    private fun verificationFile(file: MultipartFile): String {
+        if (file.isEmpty || file.originalFilename == null) throw EmptyFileException
+        val originalFilename = file.originalFilename!!
+        val ext = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).lowercase(Locale.getDefault())
+
+        if (!(ext == "jpg" || ext == "jpeg" || ext == "png" || ext == "heic")) throw BadFileExtensionException
+        return ext
+    }
+}
