9ef516b7a9b8a4b934155d4b2c880bb40a19e15f
도경 <dokyungkim2926@gmail.com>
2024-03-10 16:40:17 +0900

    add :: aws config

--- src/main/kotlin/hs/kr/equus/feed/global/config/AwsConfig.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/config/AwsConfig.kt
@@ -1,6 +1,12 @@
 package hs.kr.equus.feed.global.config
 
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
