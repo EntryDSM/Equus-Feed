c0660863a3b91f2a543a50e5c5e013ce7a137d4c
도경 <dokyungkim2926@gmail.com>
2024-03-10 21:06:40 +0900

    modify :: request

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/request/CreateNoticeRequest.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/request/CreateNoticeRequest.kt
@@ -7,17 +7,17 @@ import javax.validation.constraints.Size
 
 data class CreateNoticeRequest(
 
-    @NotBlank(message = "title은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @field:NotBlank(message = "title은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Size(max = 100, message = "title은 최대 100자까지 가능합니다.")
     val title: String,
 
-    @NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @field:NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Size(max = 5000, message = "content은 최대 5000자까지 가능합니다.")
     val content: String,
 
-    @NotNull
+    @field:NotNull
     val isPinned: Boolean,
 
-    @NotNull
+    @field:NotNull
     val type: NoticeType
 )
