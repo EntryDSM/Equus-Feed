0940fed9b56e1650d682451b3ec9734a29cd5b4d
도경 <dokyungkim2926@gmail.com>
2024-03-10 22:13:11 +0900

    add :: query title api

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/response/QueryNoticeTitleResponse.kt
@@ -0,0 +1,8 @@
+package hs.kr.equus.feed.domain.notice.presentation.dto.response
+
+import java.util.UUID
+
+data class QueryNoticeTitleResponse (
+    val id: UUID,
+    val title: String
+)
---
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/service/QueryNoticeTitleService.kt
@@ -0,0 +1,23 @@
+package hs.kr.equus.feed.domain.notice.service
+
+import hs.kr.equus.feed.domain.notice.domain.repository.NoticeRepository
+import hs.kr.equus.feed.domain.notice.presentation.dto.response.QueryNoticeTitleResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class QueryNoticeTitleService(
+    private val noticeRepository: NoticeRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun execute(): List<QueryNoticeTitleResponse> =
+        noticeRepository.findAll()
+            .map {
+                it ->
+                QueryNoticeTitleResponse(
+                    id = it.id!!,
+                    title = it.title
+                )
+            }
+}
