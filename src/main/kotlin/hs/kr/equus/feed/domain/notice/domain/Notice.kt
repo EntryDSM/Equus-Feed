8bb2cbc682fc5427ef4732c87798beb843bf5ad2
도경 <dokyungkim2926@gmail.com>
2024-03-10 15:56:16 +0900

    add :: entity

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/domain/Notice.kt
@@ -0,0 +1,32 @@
+package hs.kr.equus.feed.domain.notice.domain
+
+import hs.kr.equus.feed.domain.BaseEntity
+import hs.kr.equus.feed.domain.notice.domain.type.NoticeType
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity(name = "tbl_notice")
+class Notice (
+    id: UUID? = null,
+
+    @Column(name = "title", length = 100, nullable = false)
+    val title: String,
+
+    @Column(name = "content", length = 5000, nullable = false)
+    val content: String,
+
+    val imageUrl: String,
+
+    val fileUrl: String,
+
+
+    @Column(name = "admin_name", nullable = false)
+    val adminName: String,
+
+    val pick: Boolean,
+
+    @Column(nullable = false)
+    val type: NoticeType
+
+): BaseEntity(id)
