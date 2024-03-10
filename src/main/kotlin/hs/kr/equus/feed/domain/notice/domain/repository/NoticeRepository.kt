161af3f3ae7a7a04649bf5f2717b9ce128c23bb9
도경 <dokyungkim2926@gmail.com>
2024-03-10 15:56:26 +0900

    add :: notice repository

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/domain/repository/NoticeRepository.kt
@@ -0,0 +1,8 @@
+package hs.kr.equus.feed.domain.notice.domain.repository
+
+import hs.kr.equus.feed.domain.notice.domain.Notice
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface NoticeRepository: JpaRepository<Notice, UUID> {
+}
