ea393708a6df12bb0db4844da21e8155a8335535
도경 <dokyungkim2926@gmail.com>
2024-03-10 16:00:13 +0900

    modify :: column

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/domain/Notice.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/domain/Notice.kt
@@ -5,6 +5,8 @@ import hs.kr.equus.feed.domain.notice.domain.type.NoticeType
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Entity(name = "tbl_notice")
 class Notice (
@@ -16,17 +18,21 @@ class Notice (
     @Column(name = "content", length = 5000, nullable = false)
     val content: String,
 
+    @Column(name = "image_url", nullable = false)
     val imageUrl: String,
 
+    @Column(name = "file_url",nullable = false)
     val fileUrl: String,
 
 
     @Column(name = "admin_name", nullable = false)
     val adminName: String,
 
+    @Column(nullable = false)
     val pick: Boolean,
 
     @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     val type: NoticeType
 
 ): BaseEntity(id)
