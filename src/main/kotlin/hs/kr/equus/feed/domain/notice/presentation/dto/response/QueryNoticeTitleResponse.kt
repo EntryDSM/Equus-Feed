6a83551e47f72f3a55cda317233d77ad1dfcb731
도경 <dokyungkim2926@gmail.com>
2024-03-25 15:45:48 +0900

    modify :: query feed title api

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/response/QueryNoticeTitleResponse.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/response/QueryNoticeTitleResponse.kt
@@ -1,8 +1,11 @@
 package hs.kr.equus.feed.domain.notice.presentation.dto.response
 
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class QueryNoticeTitleResponse(
     val id: UUID,
-    val title: String
+    val title: String,
+    val createAt: LocalDateTime
 )
