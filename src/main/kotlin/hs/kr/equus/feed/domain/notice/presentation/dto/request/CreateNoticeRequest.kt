092a0abf7860e7148d318c809c212bdd2546bf78
도경 <dokyungkim2926@gmail.com>
2024-04-09 22:29:55 +0900

    modify :: create notice request

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/request/CreateNoticeRequest.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/request/CreateNoticeRequest.kt
@@ -15,10 +15,10 @@ data class CreateNoticeRequest(
     @Size(max = 5000, message = "content은 최대 5000자까지 가능합니다.")
     val content: String,
 
-    @field:NotNull
+    @field:NotNull(message = "Pinned은 null일수가 없습니다")
     val isPinned: Boolean,
 
-    @field:NotNull
+    @field:NotNull(message = "type은 null일수가 없습니다")
     val type: NoticeType,
 
     val fileName: String
