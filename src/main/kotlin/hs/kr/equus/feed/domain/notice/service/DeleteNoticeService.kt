2f7c86d57a84a93ea281ef0389855a07edd5c7db
도경 <dokyungkim2926@gmail.com>
2024-04-11 15:50:58 +0900

    ktlint

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/service/DeleteNoticeService.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/service/DeleteNoticeService.kt
@@ -1,9 +1,7 @@
 package hs.kr.equus.feed.domain.notice.service
 
-import hs.kr.equus.feed.domain.notice.domain.Notice
 import hs.kr.equus.feed.domain.notice.domain.repository.NoticeRepository
 import hs.kr.equus.feed.domain.notice.exception.NoticeNotFoundException
-import hs.kr.equus.feed.infrastructure.s3.service.S3Service
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,7 +10,6 @@ import java.util.UUID
 @Service
 class DeleteNoticeService(
     private val noticeRepository: NoticeRepository,
-    private val s3Service: S3Service
 ) {
     @Transactional
     fun execute(noticeId: UUID) {
