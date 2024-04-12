5115f8917fd32184a641ebb5605a2dc6827e1ce4
도경 <dokyungkim2926@gmail.com>
2024-04-12 22:08:58 +0900

    ktlint

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/response/QueryNoticeResponse.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/dto/response/QueryNoticeResponse.kt
@@ -4,7 +4,7 @@ import hs.kr.equus.feed.domain.notice.domain.type.NoticeType
 import java.time.LocalDateTime
 import java.util.UUID
 
-data class QueryNoticeResponse (
+data class QueryNoticeResponse(
     val notices: List<NoticeDto>
 )
 
