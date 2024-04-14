ab6f96b1cf54b0ae3b0b32234d7f19d7e2701e07
도경 <dokyungkim2926@gmail.com>
2024-04-14 21:54:39 +0900

    refactor

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/NoticeController.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/presentation/NoticeController.kt
@@ -22,15 +22,9 @@ class NoticeController(
     private val uploadNoticeImageService: UploadNoticeImageService,
     private val updateNoticeService: UpdateNoticeService,
     private val queryNoticeTitleService: QueryNoticeTitleService,
-<<<<<<< HEAD
     private val queryNoticeListByTypeService: QueryNoticeListByTypeService,
     private val getNoticeService: GetNoticeService,
-    private val deleteNoticeService: DeleteNoticeService
-=======
     private val deleteNoticeService: DeleteNoticeService,
-    private val queryNoticeListByTypeService: QueryNoticeListByTypeService,
-    private val getNoticeService: GetNoticeService
->>>>>>> develop
 ) {
 
     @ResponseStatus(value = HttpStatus.CREATED)
