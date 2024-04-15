83604785928383428d02a26b1d5776f297b2ca03
도경 <dokyungkim2926@gmail.com>
2024-04-15 20:06:02 +0900

    add :: query api

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/screen/presentation/ScreenController.kt
@@ -0,0 +1,33 @@
+package hs.kr.equus.feed.domain.screen.presentation
+
+import hs.kr.equus.feed.domain.screen.presentation.dto.response.QueryScreenResponse
+import hs.kr.equus.feed.domain.screen.presentation.dto.response.ScreenResponse
+import hs.kr.equus.feed.domain.screen.service.CreateScreenService
+import hs.kr.equus.feed.domain.screen.service.QueryScreenService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/screen")
+class ScreenController(
+    private val createScreenService: CreateScreenService,
+    private val queryScreenService: QueryScreenService
+) {
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping
+    fun createScreen(
+        @RequestPart(name = "image") image: MultipartFile
+    ): ScreenResponse =
+        createScreenService.execute(image)
+
+    @GetMapping
+    fun queryScreen(): List<QueryScreenResponse> =
+        queryScreenService.execute()
+}
---
+++ src/main/kotlin/hs/kr/equus/feed/domain/screen/service/QueryScreenService.kt
@@ -0,0 +1,28 @@
+package hs.kr.equus.feed.domain.screen.service
+
+import hs.kr.equus.feed.domain.screen.domain.repository.ScreenRepository
+import hs.kr.equus.feed.domain.screen.presentation.dto.response.QueryScreenResponse
+import hs.kr.equus.feed.infrastructure.s3.PathList
+import hs.kr.equus.feed.infrastructure.s3.util.FileUtil
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.stream.Collectors
+
+@Service
+class QueryScreenService(
+    private val screenRepository: ScreenRepository,
+    private val fileUtil: FileUtil
+) {
+    @Transactional(readOnly = true)
+    fun execute(): List<QueryScreenResponse>
+        = screenRepository.findAll()
+            .map { it ->
+                QueryScreenResponse(
+                    it.id!!,
+                    fileUtil.generateObjectUrl(it.image, PathList.SCREEN),
+                    it.createdAt,
+                    it.modifiedAt
+                )
+            }
+}
+
