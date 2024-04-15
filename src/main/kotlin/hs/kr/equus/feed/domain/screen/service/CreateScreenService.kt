69c236bb94de95cd7b7ae4586172d62547ffff28
도경 <dokyungkim2926@gmail.com>
2024-04-15 19:08:51 +0900

    add :: api

---
+++ src/main/kotlin/hs/kr/equus/feed/domain/screen/service/CreateScreenService.kt
@@ -0,0 +1,32 @@
+package hs.kr.equus.feed.domain.screen.service
+
+import hs.kr.equus.feed.domain.screen.domain.Screen
+import hs.kr.equus.feed.domain.screen.domain.repository.ScreenRepository
+import hs.kr.equus.feed.global.utils.user.UserUtils
+import hs.kr.equus.feed.infrastructure.s3.PathList
+import hs.kr.equus.feed.infrastructure.s3.util.FileUtil
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
+
+@Service
+class CreateScreenService(
+    private val screenRepository: ScreenRepository,
+    private val fileUtil: FileUtil,
+    private val userUtils: UserUtils
+) {
+
+    @Transactional
+    fun execute(file: MultipartFile): String {
+        val adminId = userUtils.getCurrentUser().id
+
+        val fileName = fileUtil.upload(file, PathList.NOTICE)
+        screenRepository.save(
+            Screen(
+                image = fileName,
+                adminId =  adminId
+            )
+        )
+        return fileName
+    }
+}
