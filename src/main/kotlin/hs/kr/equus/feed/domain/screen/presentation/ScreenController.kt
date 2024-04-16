ecc2148acb42abc0f2b23cea6d3ba286c4f52c57
도경 <dokyungkim2926@gmail.com>
2024-04-16 12:06:27 +0900

    add :: api

--- src/main/kotlin/hs/kr/equus/feed/domain/screen/presentation/ScreenController.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/screen/presentation/ScreenController.kt
@@ -4,19 +4,24 @@ import hs.kr.equus.feed.domain.screen.presentation.dto.response.QueryScreenRespo
 import hs.kr.equus.feed.domain.screen.presentation.dto.response.ScreenResponse
 import hs.kr.equus.feed.domain.screen.service.CreateScreenService
 import hs.kr.equus.feed.domain.screen.service.QueryScreenService
+import hs.kr.equus.feed.domain.screen.service.UpdateScreenService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @RestController
 @RequestMapping("/screen")
 class ScreenController(
     private val createScreenService: CreateScreenService,
+    private val updateScreenService: UpdateScreenService,
     private val queryScreenService: QueryScreenService
 ) {
 
@@ -26,6 +31,13 @@ class ScreenController(
         @RequestPart(name = "image") image: MultipartFile
     ): ScreenResponse =
         createScreenService.execute(image)
+
+    @PatchMapping("/{screen-id}")
+    fun updateScreen(
+        @PathVariable(name = "screen-id") id: UUID,
+        @RequestPart(name = "image") image: MultipartFile
+    ): ScreenResponse =
+        updateScreenService.execute(id, image)
 
     @GetMapping
     fun queryScreen(): List<QueryScreenResponse> =
