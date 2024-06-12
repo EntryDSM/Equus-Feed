4705cfaf195bf215280e952a073dd1850da9a876
HyunSu1768 <azxcv1768@gmail.com>
2024-06-12 09:00:39 +0900

    bug :: 버그수정

--- src/main/kotlin/hs/kr/equus/feed/global/utils/user/UserUtils.kt
+++ src/main/kotlin/hs/kr/equus/feed/global/utils/user/UserUtils.kt
@@ -1,5 +1,6 @@
 package hs.kr.equus.feed.global.utils.user
 
+import hs.kr.equus.feed.global.exception.InvalidTokenException
 import hs.kr.equus.feed.infrastructure.feign.client.user.UserFeignClient
 import hs.kr.equus.feed.infrastructure.feign.client.user.model.User
 import org.springframework.security.core.context.SecurityContextHolder
@@ -10,6 +11,12 @@ import java.util.UUID
 class UserUtils(
     private val userFeignClient: UserFeignClient
 ) {
-    fun getCurrentUserId(): UUID = UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+    fun getCurrentUserId(): UUID {
+        try {
+            return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+        } catch (e: IllegalArgumentException) {
+            throw InvalidTokenException
+        }
+    }
     fun getCurrentUser(): User = userFeignClient.getUserByUUID(getCurrentUserId())
 }
