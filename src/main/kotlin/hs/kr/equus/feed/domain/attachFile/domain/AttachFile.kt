91295749db6361f8fb38f5b52a76826733fb9531
도경 <dokyungkim2926@gmail.com>
2024-06-30 23:12:40 +0900

    modify :: entity

--- src/main/kotlin/hs/kr/equus/feed/domain/attachFile/domain/AttachFile.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/attachFile/domain/AttachFile.kt
@@ -3,9 +3,11 @@ package hs.kr.equus.feed.domain.attachFile.domain
 import hs.kr.equus.feed.domain.BaseEntity
 import java.util.UUID
 import javax.persistence.Entity
+import javax.persistence.Id
 
 @Entity(name = "tbl_attach_file")
 class AttachFile(
-    id: UUID? = null,
+    @Id
+    val id: String,
     var attachFileName: String,
-) : BaseEntity(id)
+)
