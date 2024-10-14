8261874cec1ffe66daa350f08a922e8b20e19f0c
도경 <dokyungkim2926@gmail.com>
2024-10-14 19:59:42 +0900

    sorted

--- src/main/kotlin/hs/kr/equus/feed/domain/notice/service/QueryNoticeTitleService.kt
+++ src/main/kotlin/hs/kr/equus/feed/domain/notice/service/QueryNoticeTitleService.kt
@@ -20,5 +20,5 @@ class QueryNoticeTitleService(
                     title = it.title,
                     it.createdAt
                 )
-            }
+            }.sortedByDescending { it.createdAt }
 }
